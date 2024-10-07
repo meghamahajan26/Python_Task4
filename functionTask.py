26eb358d1b08a767f24a5f558f023698f9dafd86
meghamahajan26 <meghamahajan8625@gmail.com>
2024-10-07 09:10:23 +0530

    examples of function

---
+++ functionTask.py
@@ -0,0 +1,22 @@
+
+# 1. Addition Function
+
+def add_numbers(a, b):
+    print(f'a = {a}')
+    print(f'b = {b}')
+    return a + b
+
+result = add_numbers(10, 5)
+
+print(f"The sum of 10 and 5 is: {result}")
+
+# 2. Check if Num is even or odd
+
+def is_even(n):
+    return n % 2 == 0
+
+result = is_even(13)
+print(result) 
+
+
+
